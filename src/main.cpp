42f6e9340ad2bce924e2b78f966816890cc5218f
Jadpoet <jadepoet0231@gmail.com>
2025-10-15 23:00:28 +0800

    fix: 新增引數與優化I/O

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
 #include "d_array.hpp"
 #include <chrono>
 #include <cmath>
+#include <fstream>
 #include <iostream>
 #include <random>
+#include <string>
 #include <vector>
 
-#define INSERT_TIME 11
+int main(int argc, char *argv[]) {
 
-int main() {
-	long long insert_time = pow(2, INSERT_TIME);
+	if ( argc != 3 ) {
+		std::cout << "ERROR ARGC NUMBER\n";
+		return 0;
+	}
+
+	enum STRUCT_TYPE { D_ARRAY, S_ARRAY, LLPP };
+	STRUCT_TYPE type;
+	std::string arg1 = argv[1];
+	if ( arg1 == "1" ) {
+		type = D_ARRAY;
+	}
+	else if ( arg1 == "2" ) {
+		type = S_ARRAY;
+	}
+	else if ( arg1 == "3" ) {
+		type = LLPP;
+	}
+	else {
+		std::cout << "ERROR ARGUMENTS\n";
+		return 0;
+	}
+
+	long long insert_time = pow(2, std::stoi(argv[2]));
 
 	std::cin.tie(0);
 	std::ios_base::sync_with_stdio(false);
@@ -18,44 +41,68 @@ int main() {
 	// Define a distribution (e.g., uniform integer distribution)
 	std::uniform_int_distribution<> distrib(0,
 	                                        100); // Numbers between 1 and 100
-	std::uniform_int_distribution<> rand_id(0,
-	                                        2048); // Numbers between 1 and 100
+	const size_t ID_SIZE = 1ULL << 20;
+	std::uniform_int_distribution<> rand_id(
+	    0,
+	    ID_SIZE); // Numbers between 1 and 2^20
 	std::chrono::steady_clock::time_point startTime =
 	    std::chrono::steady_clock::now();
-	std::cout << "-- insert start --" << std::endl;
+	std::cout << "-- insert start --\n";
 
 	dynamic_array a;
 	for ( long long i = 0; i < insert_time; i++ ) {
 		a.insert(rand_id(gen), distrib(gen));
 		// std::cout << i << std::endl;
 	}
-	std::cout << "-- insert finish --" << std::endl;
+	std::cout << "-- insert finish --\n";
 
-	std::chrono::steady_clock::time_point firstTime =
+	std::chrono::steady_clock::time_point insertFinishTime =
 	    std::chrono::steady_clock::now();
 
 	// std::vector<std::vector<int>> temp;
-	std::cout << "-- search start --" << std::endl;
+	std::cout << "-- search start --\n";
+	long long search_accumulator = 0;
 	for ( long long i = 0; i < 100000; i++ ) {
-		std::cout << i;
-		std::vector<int> temp = a.search(i);
-		for ( auto p = temp.begin(); p != temp.end(); p++ ) {
-			std::cout << " " << *p;
-		}
-		std::cout << '\n';
+		std::vector<int> temp = a.search(rand_id(gen));
+		search_accumulator += temp.size();
 	}
+	std::cout << "search_accumulator: " << search_accumulator << "\n";
 	// std::cout << temp.size() << std::endl;
-	std::cout << "--search finish --" << std::endl;
+	std::cout << "-- search finish --\n";
 
-	std::chrono::steady_clock::time_point endTime =
+	std::chrono::steady_clock::time_point searchFinishTime =
+	    std::chrono::steady_clock::now();
+
+	std::cout << "-- sum start --\n";
+
+	long long total = a.sum();
+	std::cout << "Sum: " << total << "\n";
+	std::cout << "-- sum finish --\n";
+
+	std::chrono::steady_clock::time_point sumFinishTime =
 	    std::chrono::steady_clock::now();
 
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
-	    firstTime - startTime);
-	std::cout << "time 1: " << duration.count() << " ms" << std::endl;
+	    insertFinishTime - startTime);
+	std::cout << "time 1: " << duration.count() << " ms\n";
 	auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(
-	    endTime - firstTime);
-	std::cout << "time 2: " << duration1.count() << " ms" << std::endl;
+	    searchFinishTime - insertFinishTime);
+	std::cout << "time 2: " << duration1.count() << " ms\n";
+	auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(
+	    sumFinishTime - searchFinishTime);
+	std::cout << "time 3: " << duration2.count() << " ms\n";
+
+	std::ofstream ofs;
+
+	ofs.open("result.log");
+	if ( !ofs.is_open() ) {
+		std::cout << "Failed to open file.\n";
+		return 1; // EXIT_FAILURE
+	}
+
+	ofs << arg1 << " " << std::stoi(argv[2]) << " " << duration.count() << " "
+	    << duration1.count() << " " << duration2.count() << "\n";
+	ofs.close();
 
 	return 0;
 }
